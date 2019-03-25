76b77c20e802be6a5684d9facaf45f4e938172bd
thangnguyen <huuthang9856@gmail.com>
2019-03-25 17:21:22 +0700

    First commit project

---
+++ client/javascript/test.js
@@ -0,0 +1,20 @@
+var People = {
+    name : "Hung",
+    age : 18,
+    job : 'Web developer',
+    getInfo: function () {
+        return this.name + ' - ' + this.age + ' - ' + this.job;
+    }
+};
+
+Object.defineProperty(People, "x", {
+  get() {
+    return this.stored_x;
+  },
+  set(x) {
+    this.stored_x = x;
+  }
+});
+ 
+module.exports = People; 
+  
---
+++ gulpfile.js
@@ -0,0 +1,31 @@
+const { src, dest, parallel, watch} = require('gulp');
+const minifyCSS = require('gulp-csso');
+const concat = require('gulp-concat');
+const sass = require('gulp-sass');
+const uglify = require('gulp-uglify-es').default;
+
+sass.compiler = require('node-sass');
+
+function js() {
+  return src('client/javascript/*.js')
+    .pipe(concat('app.min.js'))
+    .pipe(uglify())
+    .on('error', function (err) { console.log( err ) })
+    .pipe(dest('build/js'));
+}
+
+function scss() {
+  return src('client/scss/**/*.scss')
+    .pipe(sass.sync({outputStyle: 'compressed'}).on('error', sass.logError))
+    .pipe(dest('build/css'));
+}
+
+function watcher() {  
+  watch(['client/javascript/*.js'], parallel(js)); 
+  watch(['client/scss/*.scss'], parallel(scss));
+}
+
+exports.js = js;
+exports.scss = scss;
+exports.watcher = watcher;
+exports.build = parallel(js, scss);
